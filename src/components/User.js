f349c6b6886123a5d148400a8093d7ff9a7f9d95
Mark Masoumi <masoumi.mark@outlook.com>
2023-08-27 14:09:24 -0400

    Finished styling all components and elements

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,9 @@
 // User.js - This component displays the users Spotify profile info
 function User({ profileData }) {        
-    
     if(profileData){        
         return (
             <>
-                <section id="profile">
+                <section id="profile" className="profile">
                     <b>Logged in as <span id="displayName">{profileData.display_name}</span></b>
                 </section>
             </>
